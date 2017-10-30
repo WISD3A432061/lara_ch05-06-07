9672a724b7a553fc767a6321ae8d84db99fca542
陳姿穎 <roeyang1410@gmail.com>
2017-10-30 19:47:16 +0800

    修改board視圖上的按鈕，使之可超連接至score視圖

--- resources/views/board.blade.php
+++ resources/views/board.blade.php
@@ -29,7 +29,7 @@
 					<td>60</td>
 					<td>180</td>
 					<td>
-						<a class="btn btn-default btn-sm" href="#">查看學生資料</a>
+						<a class="btn btn-default btn-sm" href="{{url('/score')}}">查看學生資料</a>
 					</td>
 				</tr>
 				</tbody>
