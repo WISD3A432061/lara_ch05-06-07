e77788e411264aa1b99d2859d8734e5d43b9627a
陳姿穎 <roeyang1410@gmail.com>
2017-10-30 20:22:58 +0800

    把master視圖放進layouts

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,15 @@
+<!DOCYPE html>
+<html>
+<head>
+    <title>@yield('title')</title>
+    @section('head')
+        @include('partials.head')
+    @show
+</head>
+<body>
+@include('partials.nav')
+<div class="container">
+    @yield('content')
+</div>
+</body>
+</html>
